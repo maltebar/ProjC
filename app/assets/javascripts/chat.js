667ef3c6ca41a525a6e4e787837b99a5660fad4f
Maria Altebarmakian <m.altebarmakian@gmail.com>
2015-05-22 14:12:42 -0400

    chat fix

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -98,7 +98,7 @@ var ready = function () {
                 return;
             }
  
-            $('<div id="chatbox_' + conversation_id + '" class="chatbox" data-cid="'+conversation_id+'"></div>').appendTo(".chat_"+comment_id);
+            $(".chat_"+comment_id).append('<div id="chatbox_' + conversation_id + '" class="chatbox" data-cid="'+conversation_id+'"></div>')
 
             $('.chatbox').each(function() {
                 var current = $(this);
